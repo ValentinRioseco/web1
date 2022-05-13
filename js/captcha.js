f0c7c8b36371a6728a4bceba8dc0d6cd400714a2
ValentinRioseco <valentinivanr@gmail.com>
2022-05-13 01:29:36 -0300

    boseto

---
+++ js/captcha.js
@@ -0,0 +1,25 @@
+let captcha= document.querySelector("#random")
+let input= document.querySelector("#captcha")
+
+let text = "";
+
+for (let i = 0; i < 3; i++) {
+    text += String.fromCharCode(GenerarNumero(97, 122));
+    text += String.fromCharCode(GenerarNumero(48, 57));
+}
+
+function GenerarNumero(min, max) {
+    let numero = Math.floor(Math.random() * (max - min + 1) + min);
+    return numero;
+}
+captcha.innerHTML = text;
+
+document.querySelector ("#btncaptcha").addEventListener("click", (e) => { 
+    e.preventDefault()
+    if (text == input.value) {
+    document.querySelector ("#validacion").innerHTML="<p> Captcha correcto </p>"
+    }
+    else {
+    document.querySelector ("#validacion").innerHTML="<p> Captcha incorrecto </p>"
+    }
+})
